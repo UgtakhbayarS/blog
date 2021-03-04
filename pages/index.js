7106bed4b3f224ac241c87ae5bb030f1e7900ef3
Ugtakhbayar Surakhbayar <ugtakhbayars@gmail.com>
2021-03-05 02:08:59 +0800

    react bootstrap connected

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Alert from "react-bootstrap/Alert";
 
 export default function Home() {
-  return <div className={styles.container}>My blog</div>;
+  return (
+    <div className={styles.container}>
+      {["primary", "secondary", "danger", "success", "warning"].map(
+        (variant, idx) => (
+          <Alert key={idx} variant={variant}>
+            This is a {variant} alert check it out!
+          </Alert>
+        )
+      )}
+    </div>
+  );
 }
